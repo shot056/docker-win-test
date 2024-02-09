1cd24ca9e5e17fa8a393c072057182d9b7d70dd7
shot <shot056@gmail.com>
2024-02-09 19:50:38 +0900

    Update test.bat

--- test.bat
+++ test.bat
@@ -2,6 +2,7 @@
 for /f "usebackq delims=" %%A in (`git rev-parse HEAD`) do set GIT_HASH=%%A
 for /f "usebackq tokens=3 delims= " %%A in (`git status ^| findstr "On branch"`) do set GIT_BRANCH=%%A
 
+git status
 echo GIT_HASH=%GIT_HASH%
 echo GIT_BRANCH=%GIT_BRANCH%
 
